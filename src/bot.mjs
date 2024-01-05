fad2cfed7b1c036926fb8f9f9981bffa590f657a
Abduxoliq <115084319+AbduxoliqUzoqov@users.noreply.github.com>
2024-01-05 18:02:32 +0500

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,6 +1,13 @@
 import TeleBot from "telebot"
 
+
 const bot = new TeleBot("6489118017:AAFa36dFUJazpqIcRfzFm5mcUFcDxiZdjvg")
+
+bot.setMyCommands([
+    { command: "/start", description: "🔆 Botni ishga tushirish ♻️" },
+    { command: "/info", description: "🔰 Malumot olish" },
+    { command: "/admin", description: "👨‍💻 Admin Panel"},
+  ]);
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
